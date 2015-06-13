9da58b85598a9d5121654934fa47669a42436c40
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-06-13 10:10:41 +0200

    Implements online check

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var mongojs = require('mongojs');
 var extend = require('extend-object');
+var isOnline = require('is-online');
 
 function tfkSaksbehandling(options, callback){
   if (!options) {
@@ -28,27 +29,37 @@ function tfkSaksbehandling(options, callback){
     return callback(new Error('Missing required input: options.FORM_VERSION'), null);
   }
 
-  var saksbehandler = require(options.SAKSBEHANDLER);
-  var db = mongojs(options.DB);
-  var collection = db.collection(options.COLLECTION);
-
-  collection.findOne({formId:options.FORM_ID, formVersion:options.FORM_VERSION}, function(error, data) {
-    if (error) {
-      return callback(error, null);
+  isOnline(function (err, online) {
+    if (err) {
+      return callback(new Error(err), null);
     } else {
-      if (data) {
-        if (options.SAKSBEHANDLER_OPTIONS) {
-          extend(data, options.SAKSBEHANDLER_OPTIONS);
-        }
-        saksbehandler(data, function(err, result){
-          if (err) {
-            return callback(err, null);
+      if (!online) {
+        return callback(new Error('We are not online, sir.'), null);
+      } else {
+        var saksbehandler = require(options.SAKSBEHANDLER);
+        var db = mongojs(options.DB);
+        var collection = db.collection(options.COLLECTION);
+
+        collection.findOne({formId:options.FORM_ID, formVersion:options.FORM_VERSION}, function(error, data) {
+          if (error) {
+            return callback(error, null);
           } else {
-            return callback(null, result);
+            if (data) {
+              if (options.SAKSBEHANDLER_OPTIONS) {
+                extend(data, options.SAKSBEHANDLER_OPTIONS);
+              }
+              saksbehandler(data, function(err, result){
+                if (err) {
+                  return callback(err, null);
+                } else {
+                  return callback(null, result);
+                }
+              });
+            } else {
+              return callback(null, "None found");
+            }
           }
         });
-      } else {
-        return callback(null, "None found");
       }
     }
   });
